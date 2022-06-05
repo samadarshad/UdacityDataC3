27c432f678ad9b0eb0875f035a5fca4030a3e90f
Samad Arshad <samad.arshad@ev.energy>
2022-06-05 15:51:07 +0300

    Create basic table

--- sql_queries.py
+++ sql_queries.py
@@ -7,35 +7,86 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS times"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE staging_events (
+
+)
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE staging_songs (
+
+)
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP NOT NULL,
+    user_id INT NOT NULL,
+    level VARCHAR(4),
+    song_id VARCHAR(18),
+    artist_id VARCHAR(18),
+    session_id INT,
+    location VARCHAR(128),
+    user_agent VARCHAR(256),
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (song_id) REFERENCES songs (song_id),
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    );
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR(128),
+    last_name VARCHAR(128),
+    level VARCHAR(4),
+    gender VARCHAR(1)
+    );
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id VARCHAR(18) PRIMARY KEY,
+    title VARCHAR(128) NOT NULL,
+    artist_id VARCHAR(18),
+    year INT,
+    duration DOUBLE PRECISION NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    );
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR(18) PRIMARY KEY,
+    name VARCHAR(128) NOT NULL,
+    location VARCHAR(128),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION
+    );
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS times (
+    start_time TIME UNIQUE,
+    hour INT CHECK (hour >= 0 AND hour <= 23),
+    day INT CHECK (day >= 1 AND day <= 31),
+    week INT CHECK (week >= 1 AND week <= 52),
+    month INT CHECK (month >= 1 AND month <= 12),
+    year INT,
+    weekday INT CHECK (weekday >= 0 AND weekday <= 6)
+    );
 """)
 
 # STAGING TABLES
